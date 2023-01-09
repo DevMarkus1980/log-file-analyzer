13148bf0ff6dcce6187bc8d972c8e9cdd2401185
M@4rk$ <masterchef_101@outlook.de>
2023-01-09 13:47:55 +0100

    first commit

---
+++ src/main/java/de/struma/LogFileAnalyzer/LogFileAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package de.struma.LogFileAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogFileAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogFileAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/struma/LogFileAnalyzer/LogFileAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.struma.LogFileAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogFileAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
